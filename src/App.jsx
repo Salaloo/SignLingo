57d5e0f1d2106700b5a23c89bb8612f6788efabd
Salaloo <mbhuiyan8596@bths.edu>
2024-02-10 10:24:43 -0500

    removed useless

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,6 @@
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import logo from '/SignLingo.png'
-
-import Leap from 'leapjs';
 import './App.css'
 
 function App() {
